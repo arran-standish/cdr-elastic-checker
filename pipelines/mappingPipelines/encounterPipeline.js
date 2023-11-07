21128662d6f19fa0b6295d6d30e3375a9540e473
Arran Standish <arranstandish@hotmail.com>
2023-11-07 07:54:58 +0200

    Add validation to encounters

--- pipelines/mappingPipelines/encounterPipeline.js
+++ pipelines/mappingPipelines/encounterPipeline.js
@@ -21,6 +21,17 @@ export class EncounterPipeline extends BasePipeline {
 
     if (!this.patients.has(patientId) || data.type[0].coding[0].code !== "390906007") return;
 
-    this.store.setOrIncrementKey(patientId, -1);
+    let isValidEncounter = false;
+    if (Object.isKeyPopulated(data, 'extension')) {
+      for (const extension of data.extension) {
+        if (
+          Object.isKeyPopulated(extension, 'valueDateTime') || 
+          Object.isKeyPopulated(extension, 'valueString')
+        ) isValidEncounter = true;
+      }
+    }
+    if (Object.isKeyPopulated(data, 'period.start')) isValidEncounter = true;
+    
+    if (isValidEncounter) this.store.setOrIncrementKey(patientId, -1);
   }
 }
